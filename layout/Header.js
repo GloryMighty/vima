55f573885347ef3a369cb5959d769426beee2c79
GloryMighty <viacheslav@brightgridai.com>
2025-02-23 21:21:37 +0300

    Almost unfucked menu bar

--- layout/Header.js
+++ layout/Header.js
@@ -42,57 +42,55 @@ const Header1 = ({ homepage }) => {
   };
 
   return (
-    <header className="navbar">
-      <div className="container">
-        <div className="row">
-          <nav className="navbar navbar-expand-lg tf__main_menu main_menu" style={{ padding: '0', margin: '0' }}>
-            <div className={`container${homepage ? "-fluid" : ""}`}>  
-              <Link className="navbar-brand" href="/">
-                <img src="images/logo.png" alt="logo" className="img-fluid w-100" />
-              </Link>
-              <button
-                className="navbar-toggler"
-                type="button"
-                onClick={() => {
-                  console.log("Button clicked, current state:", isMobileMenuOpen);
-                  setMobileMenuOpen(!isMobileMenuOpen);
-                }}
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <i className="far fa-stream menu_icon_bar" />
-                <i className="far fa-times close_icon_close" />
-              </button>
-              <div className={`collapse navbar-collapse ${isMobileMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
-                <ul className={`navbar-nav ms-auto ${isMobileMenuOpen ? 'open flex-column' : 'd-none d-md-flex'}`}>  
-                  <li className={`nav-item ${isActiveLink('/') ? 'active' : ''}`}>  
-                    <Link href="/" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/')}>{t('HOME')}</Link>
-                  </li>
-                  <li className={`nav-item ${isActiveLink('/about') ? 'active' : ''}`}>  
-                    <Link href="/about" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/about')}>{t('ABOUT')}</Link>
-                  </li>
-                  <li className={`nav-item ${isActiveLink('/pricing') ? 'active' : ''}`}>  
-                    <Link href="/pricing" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/pricing')}>{t('PRICING')}</Link>
-                  </li>
-                  <li className={`nav-item ${isActiveLink('/blog') ? 'active' : ''}`}>  
-                    <Link href="/blog" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/blog')}>{t('BLOG')}</Link>
-                  </li>
-                  <li className={`nav-item ${isActiveLink('/contact') ? 'active' : ''}`}>  
-                    <Link href="/contact" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/contact')}>{t('CONTACT')}</Link>
-                  </li>
-                </ul>
-                <div className="language-selector-container">
-                  <LanguageSelector />
-                </div>
+    <div className="container">
+      <div className="row">
+        <nav className="navbar navbar-expand-lg tf__main_menu main_menu">
+          <div className={`container${homepage ? "-fluid" : ""}`}>  
+            <Link className="navbar-brand" href="/">
+              <img src="images/logo.png" alt="logo" className="img-fluid w-100" />
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              onClick={() => {
+                console.log("Button clicked, current state:", isMobileMenuOpen);
+                setMobileMenuOpen(!isMobileMenuOpen);
+              }}
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <i className="far fa-stream menu_icon_bar" />
+              <i className="far fa-times close_icon_close" />
+            </button>
+            <div className={`collapse navbar-collapse ${isMobileMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
+              <ul className={`navbar-nav ms-auto ${isMobileMenuOpen ? 'open flex-column' : 'd-none d-md-flex'}`}>  
+                <li className={`nav-item ${isActiveLink('/') ? 'active' : ''}`}>  
+                  <Link href="/" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/')}>{t('HOME')}</Link>
+                </li>
+                <li className={`nav-item ${isActiveLink('/about') ? 'active' : ''}`}>  
+                  <Link href="/about" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/about')}>{t('ABOUT')}</Link>
+                </li>
+                <li className={`nav-item ${isActiveLink('/pricing') ? 'active' : ''}`}>  
+                  <Link href="/pricing" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/pricing')}>{t('PRICING')}</Link>
+                </li>
+                <li className={`nav-item ${isActiveLink('/blog') ? 'active' : ''}`}>  
+                  <Link href="/blog" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/blog')}>{t('BLOG')}</Link>
+                </li>
+                <li className={`nav-item ${isActiveLink('/contact') ? 'active' : ''}`}>  
+                  <Link href="/contact" className="nav-link text_hover_animaiton" onClick={() => handleNavigation('/contact')}>{t('CONTACT')}</Link>
+                </li>
+              </ul>
+              <div className="language-selector-container">
+                <LanguageSelector />
               </div>
             </div>
-          </nav>
-        </div>
+          </div>
+        </nav>
       </div>
-    </header>
+    </div>
   );
 };
 
