dffbf6c9c4a28df9293f7fdb703bbd123b67d558
GloryMighty <viacheslav@brightgridai.com>
2025-02-23 14:08:21 +0300

    Trying to get rid of overflow

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -14,13 +14,14 @@ const page = () => {
   const t = useTranslations('HomePage');
 
   return (
-    <IstiLayout homepage>
+    <IstiLayout homepage className="overflow-hidden">
       <ClientHelmet 
         title={`${t('meta_title')} | Vima Web Solutions`}
         description={t('meta_description')}
         keywords={t('meta_keywords')}
       />
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="google-site-verification" content="SehANDDhj3LTVodEsYzH4X4CN6LMUH_q_KvNrMaZA9o" />
         <meta name="google-site-verification" content="google-search-console-verification-token" />
       </Head>
