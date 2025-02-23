c89f1630b682f28434f189471066e423ebcc606c
GloryMighty <viacheslav@brightgridai.com>
2025-02-23 13:12:34 +0300

    Test applying common css to banner

--- app/Banner.js
+++ app/Banner.js
@@ -8,17 +8,16 @@ export const Home1Banner = () => {
       style={{ background: "url(images/background.jpg)" }}
     >
       <div className="container py-4">
-        <div className="row align-items-center">
-          <div className="col-xxl-5 col-xl-6 ms-auto">
-            <div className="tf__banner_text">
-              <h3 className="banner_title" data-text-animation="slide-up" data-split="char">
-                {t('we_are_vima')}
-              </h3>
-              <h2 className="banner_title">
-                {t('passionate_website_developers')}
-              </h2>
-              <p className="banner_title">
-                {t('banner_paragraph')}
+        <div className="col-xxl-5 col-xl-6 ms-auto">
+          <div className="tf__banner_text">
+            <h3 className="tf__banner_text" data-text-animation="slide-up" data-split="char">
+              {t('we_are_vima')}
+            </h3>
+            <h2 className="tf__banner_text">
+              {t('passionate_website_developers')}
+            </h2>
+            <p className="tf__banner_text">
+              {t('banner_paragraph')}
               </p>
               <ul className="d-flex flex-wrap">
                 <li>
@@ -34,7 +33,6 @@ export const Home1Banner = () => {
             </div>
           </div>
         </div>
-      </div>
     </section>
   );
 };
