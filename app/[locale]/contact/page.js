9a0d5172f7d2236603160a1a5e629878d3f22866
DnaKerma <dnakerma@gmail.com>
2025-04-29 10:30:47 +0300

    SmallChanges2904

--- app/[locale]/contact/page.js
+++ app/[locale]/contact/page.js
@@ -81,7 +81,7 @@ ${message}`;
       type: t('offices.sales.type'),
       location: t('offices.sales.location'),
       phones: [
-        <p key="0"><Link href="tel:+3584578753412">+358 45 7875 3412</Link></p>,
+        <p key="1"><Link href="tel:+3584578753412">+358 45 7875 3412</Link></p>,
               ]
     },
     {
@@ -90,7 +90,7 @@ ${message}`;
       type: t('offices.sales2.type'),
       location: t('offices.sales2.location'),
       phones: [
-        <p key="0"><Link href="tel:+3584578753416">+358 45 7875 3416</Link></p>,
+        <p key="2"><Link href="tel:+3584578753416">+358 45 7875 3416</Link></p>,
               ]
     }
   ];
